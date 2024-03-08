88f01774cf83f3050e979a8d4bfa2b264a419e30
Ramón <ramon.mosquera@gmail.com>
2024-03-08 17:40:45 +0100

    Module constructor

---
+++ modules/multipurpose/multipurpose.php
@@ -0,0 +1,22 @@
+<?php
+class Multipurpose extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'multipurpose';
+        $this->author = 'Ramón Mosquera';
+        $this->version = '1.0.0';
+        parent::__construct();
+        $this->displayName = $this->l('Multipurpose');
+        $this->description = $this->l('This is part of the Prestashop module development.');
+        $this->ps_versions_compliancy = ['min' => '8.0.0', 'max' => '8.99.99'];
+    }
+    public function install()
+    {
+        return parent::install();
+    }
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+}
